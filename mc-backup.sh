b90df03d44ac89dd520a7b5c15a26e079a8040b3
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2022-03-05 14:46:09 -0300

    Avoid variable gobling & (#2)
    Change grep ps to pgrep

--- mc-backup.sh
+++ mc-backup.sh
@@ -3,7 +3,7 @@
 MC-BACKUP
 https://github.com/J-Bentley/mc-backup.sh'
 
-pathFile=$(readlink -f $BASH_SOURCE)
+pathFile=$(readlink -f "$BASH_SOURCE")
 dirname=${pathFile%/*}
 
 required_files=(constants.sh)
@@ -26,13 +26,13 @@ log () {
 
 for v in ${required_files[*]}; { [[ -r "$dirname"/${v} ]] || { echo "File ${v} not found. Exiting..."; $exit; }; }
 
-source $dirname/constants.sh
+source "$dirname"/constants.sh
 
 worldfoldercheck () {
     # Checks to make sure all the worlds defined in serverWorlds array exist as directories
     for item in "${serverWorlds[@]}"
     do
-        if [ ! -d $backupDir/$item ]; then
+        if [ ! -d "$backupDir"/"$item" ]; then
             log "[$currentDay] Error: World folder not found! Backup has been cancelled. ($backupDir/$item)\n"
             $exit 1
 	fi
@@ -40,9 +40,9 @@ worldfoldercheck () {
 }
 deleteBackup() {
     # Deletes contents of backupDir
-    if [ "$(ls -A $backupDir)" ]; then
+    if [ "$(ls -A "$backupDir")" ]; then
 	log "[$currentDay] Warning: Backup directory not empty! Deleting contents before proceeding...\n"
-        rm -R ${backupDir:?}/*
+        rm -R "${backupDir:?}"/*
         $exit 1
     fi
 }
@@ -91,17 +91,17 @@ if [ $# -gt 1 ]; then
     $exit 1
 fi
 # Logs error and cancels script if serverDir isn't found
-if [ ! -d $serverDir ]; then
+if [ ! -d "$serverDir" ]; then
     log "[$currentDay] Error: Server folder not found! Backup has been cancelled. ($serverDir)\n"
     $exit 1
 fi
 # Logs error and cancels script if backupDir isn't found
-if [ ! -d $backupDir ]; then
+if [ ! -d "$backupDir" ]; then
     log "[$currentDay] Error: Backup folder not found! Backup has been cancelled. ($backupDir)\n"
     $exit 1
 fi
 # Logs error if JAVA process isn't detected but will continue anyways!!
-if ! ps -e | grep -q "java"; then
+if ! pgrep -u "$minecraftUser" "java"; then
     log "[$currentDay] Warning: $serverName is not running! Continuing without in-game warnings...\n"
     serverRunning=false
 fi
@@ -115,7 +115,7 @@ fi
 if $serverRunning; then
     log "[$currentDay] Server is running. Stopping it..."
     # Restart the server logic
-    source $dirname/mc-manage-server.sh
+    source "$dirname"/mc-manage-server.sh
     
     stopMessage
     log "[$currentDay] Sent players message"
@@ -132,23 +132,23 @@ elapsedTimeStart="$(date -u +%s)"
 if $worldsOnly; then
     log "[$currentDay] Worlds only started ...\n"
     # Starts the tar with files from the void (/dev/null is a symlink to a non-existent dir) so that multiple files can be looped in from array then gziped together.
-    sudo -u $minecraftUser tar cf $backupDir/"$serverName"[WORLDS]-$currentDay.tar --files-from /dev/null
+    sudo -u "$minecraftUser" tar cf "$backupDir"/"$serverName"[WORLDS]-"$currentDay".tar --files-from /dev/null
     for item in "${serverWorlds[@]}"
     do
-        sudo -u $minecraftUser tar rf $backupDir/"$serverName"[WORLDS]-$currentDay.tar "$serverDir/$item"
+        sudo -u "$minecraftUser" tar rf "$backupDir"/"$serverName"[WORLDS]-"$currentDay".tar "$serverDir/$item"
     done
-    gzip $backupDir/"$serverName"[WORLDS]-$currentDay.tar
+    gzip "$backupDir"/"$serverName"[WORLDS]-"$currentDay".tar
 elif $pluginOnly; then
     log "[$currentDay] Plugins only started...\n"
-    sudo -u $minecraftUser tar -czPf $backupDir/"$serverName"[PLUGINS]-$currentDay.tar.gz $serverDir/plugins
+    sudo -u "$minecraftUser" tar -czPf "$backupDir"/"$serverName"[PLUGINS]-"$currentDay".tar.gz "$serverDir"/plugins
 elif $pluginconfigOnly; then
     log "[$currentDay] Plugin Configs only started...\n"
-    sudo -u $minecraftUser tar -czPf $backupDir/"$serverName"[PLUGIN-CONFIG]-$currentDay.tar.gz --exclude='*.jar' $serverDir/plugins
+    sudo -u "$minecraftUser" tar -czPf "$backupDir"/"$serverName"[PLUGIN-CONFIG]-"$currentDay".tar.gz --exclude='*.jar' "$serverDir"/plugins
 elif $startStop; then
     log "[$currentDay] Skipping backup\n"
 else
     log "[$currentDay] Full compression started...\n"
-    sudo -u $minecraftUser tar -czPf $backupDir/$serverName-$currentDay.tar.gz $serverDir
+    sudo -u "$minecraftUser" tar -czPf "$backupDir"/"$serverName"-"$currentDay".tar.gz "$serverDir"
 fi
 
 # Grabs date in seconds AFTER compression completes then does math to find time it took to compress
@@ -156,7 +156,7 @@ elapsedTimeEnd="$(date -u +%s)"
 elapsed="$(($elapsedTimeEnd-$elapsedTimeStart))"
 
 # Grabs size of item in backuplocation, assumes compressed item is only file in dir via deletebackup function
-compressedSize=$(du -sh $backupDir/*-$currentDay* | grep -P $regex)
+compressedSize=$(du -sh "$backupDir"/*-"$currentDay"* | grep -P "$regex")
 
 
 if $worldsOnly; then
@@ -169,11 +169,11 @@ elif $startStop; then
     log "[$currentDay] Restart in progress."
 else
     # Grabs size of Server file in kb for comparison on output
-    uncompressedSize=$(du -sh $serverDir | grep -P $regex)
+    uncompressedSize=$(du -sh "$serverDir" | grep -P "$regex")
     log "[$currentDay] $serverDir compressed from $uncompressedSize to $compressedSize and copied to $backupDir in $((elapsed/60)) min(s)!\n"
 fi
 
-backupFolderSize=$(du -sh $backupDir | grep -P $regex)
+backupFolderSize=$(du -sh "$backupDir" | grep -P "$regex")
 
 log "[$currentDay] The backup folder is already $backupFolderSize in size."
 
