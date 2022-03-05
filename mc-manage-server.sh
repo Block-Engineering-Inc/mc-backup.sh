b5cbda4668905b99d5e4782d57773758ee5577bd
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2022-03-05 14:29:27 -0300

    Avoid variable gobling & Change grep ps to pgrep

--- mc-manage-server.sh
+++ mc-manage-server.sh
@@ -3,17 +3,17 @@
 stopMessage () {
     # injects commands into console via stuff to warn chat of backup, sleeps for graceperiod, restarts, sleeps for hdd spin times
     log "[$currentDay] Warning players & stopping $serverName...\n"
-    sudo -u $minecraftUser screen -p 0 -X stuff "say $serverName is restarting in $gracePeriod!$(printf \\r)"
-    sleep $gracePeriod
-    sudo -u $minecraftUser screen -p 0 -X stuff "say $serverName is restarting now!$(printf \\r)"
-    sudo -u $minecraftUser screen -p 0 -X stuff "save-all$(printf \\r)"
+    sudo -u "$minecraftUser" screen -p 0 -X stuff "say $serverName is restarting in $gracePeriod!$(printf \\r)"
+    sleep "$gracePeriod"
+    sudo -u "$minecraftUser" screen -p 0 -X stuff "say $serverName is restarting now!$(printf \\r)"
+    sudo -u "$minecraftUser" screen -p 0 -X stuff "save-all$(printf \\r)"
     sleep 5
 }
 stopServer(){
-    sudo systemctl stop $serviceName
+    sudo systemctl stop "$serviceName"
     log "[$currentDay] Stopped Service $serviceName"
 }
 startServer(){
-    sudo systemctl start $serviceName
+    sudo systemctl start "$serviceName"
     log "[$currentDay] Started Service $serviceName"
 }
