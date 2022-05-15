ac2738fbac4f5970ee678f6aa0904dc4498313ef
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2022-05-15 18:53:23 -0300

    Stop the server only if correctly configured

--- mc-build-website.sh
+++ mc-build-website.sh
@@ -18,6 +18,16 @@ if ! pgrep -u "$minecraftUser" "java"; then
     serverRunning=false
 fi
 
+if [ -z "$websiteBucket" -a -z "$1" ]; then
+    log "[$currentDay] Error: No website bucket defined! Upload has been cancelled.\n"
+    $exit 1
+elif [ -z "$websiteBucket" -a -n "$1" ]; then
+    websiteBucket="$1"
+    log "[$currentDay] website bucket defined: $websiteBucket\n"
+else
+    log "[$currentDay] website bucket defined: $websiteBucket\n"
+fi
+
 # Wont do anything if server is running - Stop it
 if $serverRunning; then
     log "[$currentDay] Server is running. Stopping it..."
@@ -30,16 +40,6 @@ if $serverRunning; then
     stopServer
     log "[$currentDay] Server stopped"
     serverRunning=false
-fi
-
-if [ -z "$websiteBucket" -a -z "$1" ]; then
-    log "[$currentDay] Error: No website bucket defined! Upload has been cancelled.\n"
-    $exit 1
-elif [ -z "$websiteBucket" -a -n "$1" ]; then
-    websiteBucket="$1"
-    log "[$currentDay] website bucket defined: $websiteBucket\n"
-else
-    log "[$currentDay] website bucket defined: $websiteBucket\n"
 fi
 
 log "[$currentDay] Website: Starting the build\n"
