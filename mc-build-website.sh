7485dfad748d120485e776c3b49648b7c4bceb43
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2022-06-07 01:14:18 +0000

    Add absolute paths
    - Add quoted variables

--- mc-build-website.sh
+++ mc-build-website.sh
@@ -43,17 +43,17 @@ if $serverRunning; then
 fi
 
 log "[$currentDay] Website: Starting the build for $overviewer_site\n"
-$overviewer_path --config $overviewer_config --genpoi
-$overviewer_path --config $overviewer_config
+$overviewer_path --config "$overviewer_config" --genpoi
+$overviewer_path --config "$overviewer_config"
 
-sed -i -E 's/"icon": "(\w+\.\w+)"/"icon": baseUrl.concat("", "\1")/' markers.js
-sed -i -e '1i\var baseUrl = staticurl;' markers.js
+sed -i -E 's/"icon": "(\w+\.\w+)"/"icon": baseUrl.concat("", "\1")/' "$overviewer_site"/markers.js
+sed -i -e '1i\var baseUrl = staticurl;' "$overviewer_site"/markers.js
 
 log "[$currentDay] Website: Uploading the build\n"
-$oci_path os object put -bn $websiteBucket --file $overviewer_site/markers.js --name markers.js --force
-$oci_path os object put -bn $websiteBucket --file $overviewer_site/markersDB.js --name markersDB.js --force
-$oci_path os object bulk-upload -bn $websiteBucket --src-dir $overviewer_site/normalrender/ --overwrite --prefix "normalrender/"
-$oci_path os object bulk-upload -bn $websiteBucket --src-dir $overviewer_site/survivalnether/ --overwrite --prefix "survivalnether/"
+$oci_path os object put -bn "$websiteBucket" --file "$overviewer_site"/markers.js --name markers.js --force
+$oci_path os object put -bn "$websiteBucket" --file "$overviewer_site"/markersDB.js --name markersDB.js --force
+$oci_path os object bulk-upload -bn "$websiteBucket" --src-dir "$overviewer_site"/normalrender/ --overwrite --prefix "normalrender/"
+$oci_path os object bulk-upload -bn "$websiteBucket" --src-dir "$overviewer_site"/survivalnether/ --overwrite --prefix "survivalnether/"
 
 if ! $serverRunning; then
     # start back again
