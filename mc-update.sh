1366006676b0ab4228fcfe859168bc38d834a95e
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2022-06-10 19:46:03 -0300

    Add sudo and chown to server jar

--- mc-update.sh
+++ mc-update.sh
@@ -55,7 +55,8 @@ fi
 versionManifestUrl=$(curl --silent 'https://launchermeta.mojang.com/mc/game/version_manifest.json' | jq --arg VANILLA_VERSION "$mc_version" --raw-output '[.versions[]|select(.id == $VANILLA_VERSION)][0].url')
 mc_download_url=$(curl --silent $versionManifestUrl | jq '.downloads.server.url')
 
-wget -q -O "$serverDir"/minecraft_server.jar "$mc_download_url"
+sudo su - minecraft wget -q -O "$serverDir"/minecraft_server.jar "$mc_download_url"
+sudo chown "$minecraftUser":"$minecraftUser" "$serverDir"/minecraft_server.jar
 
 if ! $serverRunning; then
     # start back again
