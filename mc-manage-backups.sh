2a064d5c667f85b8271e8889132db7e230eb0784
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2022-06-14 20:52:18 -0300

    Add backup management script

---
+++ mc-manage-backups.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pathFile=$(readlink -f "$BASH_SOURCE")
+dirname=${pathFile%/*}
+
+required_files=(constants.sh lib.sh)
+
+for v in ${required_files[*]}; { [[ -r "$dirname"/${v} ]] || { echo "File ${v} not found. Exiting..."; $exit; }; }
+
+source "$dirname"/constants.sh
+source "$dirname"/lib.sh
+
+maxBackupCount=14
+backupFilesCount=$(ls "$backupDir" | wc -w)
+backup_files=($(ls $backupDir))
+
+log "[$currentDay] Amount of backups are at $backupFilesCount\n"
+
+if [ "$backupFilesCount" -gt "$maxBackupCount" ]; then
+    diff=$(("$backupFilesCount"-"$maxBackupCount"-1))
+    log "[$currentDay] Amount of backups have surpassed $maxBackupCount. Deleting $diff old files.\n"
+    for i in $(seq 0 $diff); do
+        file="${backup_files[i]}"
+        log "[$currentDay] Deleting $file.\n"
+    done
+fi
+
+$exit 0
